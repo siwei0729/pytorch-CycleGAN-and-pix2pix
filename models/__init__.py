fdb1cf4e690112e4e01c9f00f10a74481841cdc5
Siwei <chensiwei0729@gmail.com>
2020-01-02 16:00:24 +0800

    add multiple GPU

--- models/__init__.py
+++ models/__init__.py
@@ -19,6 +19,7 @@ See our template model class 'template_model.py' for more details.
 """
 
 import importlib
+import torch.nn as nn
 from models.base_model import BaseModel
 
 
@@ -62,6 +63,7 @@ def create_model(opt):
         >>> model = create_model(opt)
     """
     model = find_model_using_name(opt.model)
+    model = nn.DataParallel(model) #enabling data parallelism
     instance = model(opt)
     print("model [%s] was created" % type(instance).__name__)
     return instance
