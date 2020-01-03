52d4da5241e38b5e2ffdc0544f4731bc6ae52127
Siwei <chensiwei0729@gmail.com>
2020-01-03 14:24:53 +0800

    update

--- train.py
+++ train.py
@@ -28,7 +28,6 @@ if __name__ == '__main__':
     opt = TrainOptions().parse()  # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)  # get the number of images in the dataset.
-    opt.batch_size = 64
     print('The number of training images = %d' % dataset_size)
     print('batch size:', opt.batch_size)
 
